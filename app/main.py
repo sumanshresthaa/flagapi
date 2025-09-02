6b5a4a95a4fba100627a6c1ec4cfab2de5637d46
sumanshresthaa <sumanshrestha360@gmail.com>
2025-09-01 17:16:46 -0700

    deploying python2

--- app/main.py
+++ app/main.py
@@ -123,5 +123,5 @@ async def predict(file: UploadFile = File(...)):
     }
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
